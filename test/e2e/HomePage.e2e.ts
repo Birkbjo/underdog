ff6a4de84a29b740a9a59e479ecb55a8e045a931
Birk Johansson <birk@dhis2.org>
2020-07-03 03:37:06 +0200

    fix: only restart main on main.dev.ts change

--- test/e2e/HomePage.e2e.ts
+++ test/e2e/HomePage.e2e.ts
@@ -20,63 +20,9 @@ const assertNoConsoleErrors = async (t) => {
 fixture`Home Page`.page('../../app/app.html').afterEach(assertNoConsoleErrors);
 
 test('e2e', async (t) => {
-  await t.expect(getPageTitle()).eql('Hello Electron React!');
+  await t.expect(getPageTitle()).eql('Underdog Addon Manager');
 });
 
 test('should open window and contain expected page title', async (t) => {
-  await t.expect(getPageTitle()).eql('Hello Electron React!');
-});
-
-test(
-  'should not have any logs in console of main window',
-  assertNoConsoleErrors
-);
-
-test('should navigate to Counter with click on the "to Counter" link', async (t) => {
-  await t.click('[data-tid=container] > a').expect(getCounterText()).eql('0');
-});
-
-test('should navigate to /counter', async (t) => {
-  await t.click('a').expect(getPageUrl()).contains('/counter');
-});
-
-fixture`Counter Tests`
-  .page('../../app/app.html')
-  .beforeEach(clickToCounterLink)
-  .afterEach(assertNoConsoleErrors);
-
-test('should display updated count after the increment button click', async (t) => {
-  await t.click(incrementButton).expect(getCounterText()).eql('1');
-});
-
-test('should display updated count after the descrement button click', async (t) => {
-  await t.click(decrementButton).expect(getCounterText()).eql('-1');
-});
-
-test('should not change even counter if odd button clicked', async (t) => {
-  await t.click(oddButton).expect(getCounterText()).eql('0');
-});
-
-test('should change odd counter if odd button clicked', async (t) => {
-  await t
-    .click(incrementButton)
-    .click(oddButton)
-    .expect(getCounterText())
-    .eql('2');
-});
-
-test('should change if async button clicked and a second later', async (t) => {
-  await t
-    .click(asyncButton)
-    .expect(getCounterText())
-    .eql('0')
-    .expect(getCounterText())
-    .eql('1');
-});
-
-test('should back to home if back button clicked', async (t) => {
-  await t
-    .click('[data-tid="backButton"] > a')
-    .expect(Selector('[data-tid="container"]').visible)
-    .ok();
+  await t.expect(getPageTitle()).eql('Underdog Addon Manager');
 });
